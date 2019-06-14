6c6a57331f98eaca507f1f4dc6f18f669a39a5d0
brenner8023 <brenner8023@163.com>
2019-06-14 22:26:25 +0800

    编写初始页面

---
+++ src/components/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+    render() {
+        return <main className="main">
+            <div className="grid-container">
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+            </div>
+        </main>
+    }
+}
